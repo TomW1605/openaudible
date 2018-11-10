12302a8c57377ad80dca6185055d819f00a1e391
TomW1605 <tawhite@iinet.net.au>
2018-11-10 16:40:30 +0800

    Author test 2

--- src/main/java/org/openaudible/desktop/swt/manager/Prefs.java
+++ src/main/java/org/openaudible/desktop/swt/manager/Prefs.java
@@ -2,7 +2,7 @@ package org.openaudible.desktop.swt.manager;
 
 // desktop app prefs.
 public class Prefs {
-	public boolean autoConvert = true;
+	public boolean autoConvert = false;
 	public boolean autoDownload = false;
 	public boolean autoWebPage = false;
 	public boolean webPageIncludeMP3 = true;
